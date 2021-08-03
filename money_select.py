395f557377813348abe77194de245dfcb95686a1
Diego Guerrero <diegoes9801@gmail.com>
2021-08-02 22:17:31 -0500

    Trabajo de POO (clase) hacia la seleccion de la moneda

---
+++ money_select.py
@@ -0,0 +1,9 @@
+class Money_Select:
+    id = int
+    id_money_A = int
+    id_money_B = int
+
+    def __init__(self, id, id_moneda_A, id_moneda_B):
+        self.id = id 
+        self.id_moneda_A = id_moneda_A
+        self.id_moneda_B = id_moneda_B
