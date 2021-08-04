64b4f3555af0ea7d0f01e4ff1b71b561517deb82
Diego Guerrero <diegoes9801@gmail.com>
2021-08-04 02:41:55 -0500

    new commit

--- main_conversor.py
+++ main_conversor.py
@@ -1,48 +1,50 @@
-from Monedas import peso_COP
-from Monedas import peso_Arg
-from Monedas import peso_MX
-from Monedas import euro
-from Monedas import dollar
-from Monedas import libra
+
 
 import random
+from Class_menu import Menu
 from User_Conversor import User
 from Request_for_Use import Request_For_Use
 from money_select import Money_Select
 from Conversion import Conversion
 
+def menu_1():
+    Menu._id = random.randint(1, 1000)
+    Menu.mensaje = """
+     ¿Qué moneda posee?
+
+     1 - Pesos colombianos
+     2 - Pesos argentinos
+     3 - Pesos mexicanos
+     4 - Dolares
+     5 - Euros
+     6 - Libras esterlinas
+
+     elige una opción: """
+    Menu.valor_entrada = int(input(Menu.mensaje))
+
+def menu_2():
+    Menu._id = random.randint(1, 1000)
+    Menu.mensaje = """
+     ¿A qué moneda desea convertir su dinero?
+
+     1 - Pesos colombianos
+     2 - Pesos argentinos
+     3 - Pesos mexicanos
+     4 - Dolares
+     5 - Euros
+     6 - Libras esterlinas
+
+     elige una opción: """
+    Menu.valor_entrada = input(Menu.mensaje)
+
+
 def run():
-    User.name = (f'Bienvenido al conversor de Monedas, para empezar, danos tu nombre: ')
+    User.name = (input(f'Bienvenido al conversor de Monedas, para empezar, danos tu nombre: '))
     User.id = random.randint(1, 1000)
     Money_Select.id = random.randint(1, 1000)
-    print(f'Hola {User.name}, ¿qué moneda tienes para hacer tu conversión?')
-    menu_1 = """
-     1 - Pesos colombianos
-     2 - Pesos argentinos
-     3 - Pesos mexicanos
-     4 - Dolares
-     5 - Euros
-     6 - Libras esterlinas
-
-     elige una opción: """
-
-    moneda_A = int(input(menu_1))
-
-    menu_2 = """
-     ¿A qué moneda deseas hacer la conversión?
-
-     1 - Pesos colombianos
-     2 - Pesos argentinos
-     3 - Pesos mexicanos
-     4 - Dolares
-     5 - Euros
-     6 - Libras esterlinas
-
-     elige una opción: """
-
-    moneda_B = int(input(menu_2))
-
-    if menu_1 == 1 and menu_2 == 2
+    print(f'Hola {User.name}')
+    menu_1()
+    menu_2()
 
 
 if __name__ == '__main__':
