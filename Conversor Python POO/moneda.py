26b58a9ce623753feb0be352fdccd32fc780aaa4
Diego Guerrero <diegoes9801@gmail.com>
2021-08-06 03:15:32 -0500

    archivos borrados obsoletos

--- Conversor Python POO/moneda.py
+++
@@ -1,7 +0,0 @@
-class Money:
-
-    def __init__(self, id, name, valor_respecto_a):
-        self.id = id
-        self.name = name
-        self.valor_respecto_a = valor_respecto_a
-
--- Conversor Python POO/money_select.py
+++
@@ -1,9 +0,0 @@
-class Money_Select:
-    id = int
-    id_money_A = int
-    id_money_B = int
-
-    def __init__(self, id, id_moneda_A, id_moneda_B):
-        self._id = id 
-        self.id_moneda_A = id_moneda_A
-        self.id_moneda_B = id_moneda_B
